54a7b6ff755804924745c418178d591291718a2d
yyj424 <69260313+yyj424@users.noreply.github.com>
2023-12-12 23:30:11 +0900

    [level 2] Title: 피보나치 수, Time: 3.01 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        long[] fibo = new long[n + 1];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567L;
+        }
+        
+        return (int) fibo[n];
+    }
+}
